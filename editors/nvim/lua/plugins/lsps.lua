ab62a749760148133df0b7439fa0fe8e9a6c5620
lumesque <luckie.joshua.c@gmail.com>
2025-02-22 05:04:37 -0500

    Adding in python lsp
    This adds in the python lsp and allows for formatter to be ran with ruff
    during python installation, others will be installed at later points for
    other languages as we go

--- editors/nvim/lua/plugins/lsps.lua
+++ editors/nvim/lua/plugins/lsps.lua
@@ -270,6 +270,7 @@ return {
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
 				"stylua", -- Used to format Lua code
+				"ruff", -- Used to style python code
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
