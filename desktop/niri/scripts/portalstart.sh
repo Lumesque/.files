5582b5c32c1034bb5864c9404d721d56126c5ee4
lumesque <luckie.joshua.c@gmail.com>
2025-04-17 18:30:01 -0400

    Adding back in hyprland desktop portal
    There were errors with discord and screenshare when using gtk and other
    portals, so until this gets more figured out going to keep using hyprs

--- desktop/niri/scripts/portalstart.sh
+++ desktop/niri/scripts/portalstart.sh
@@ -4,9 +4,11 @@ sleep 1
 killall xdg-desktop-portal-gtk
 killall xdg-desktop-portal-wlr
 killall xdg-desktop-portal
+killall xdg-desktop-portal-hyprland
 killall mako
 sleep 2
 /usr/libexec/xdg-desktop-portal-gtk &
 /usr/libexec/xdg-desktop-portal-wlr &
 /usr/libexec/xdg-desktop-portal &
+/usr/libexec/xdg-desktop-portal-hyprland &
 mako &
